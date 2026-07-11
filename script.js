7412ffe7c84aac28dc7f780cc42d3567f652fcb8
Jacob Schaller <jschaller2028@gmail.com>
2026-07-11 13:20:57 -0400

    Added better error handling

--- script.js
+++ script.js
@@ -9,10 +9,22 @@ const file_input = document.getElementById("file-input");
 const chat_window = document.getElementById("chat-window");
 const api_key_input = document.getElementById("api-key");
 const api_key_status = document.getElementById("api-key-status");
+const send_button = document.getElementById("send-button");
+const upload_button = document.getElementById("upload-button");
+const query_status = document.getElementById("query-status");
+
+var user_message;
+var asst_message;
 
 var api_key = "";
 
 const chat = [];
+
+function delete_last_message() {
+    chat.pop();
+    chat_window.removeChild(asst_message);
+    chat_window.removeChild(user_message);
+}
 
 query_form.addEventListener("submit", async (event) => {
     event.preventDefault();
@@ -22,16 +34,17 @@ query_form.addEventListener("submit", async (event) => {
     if (data["prompt"].trim() == "") {
         return;
     }
-    var user_message = document.createElement("p");
+    user_message = document.createElement("p");
     user_message.className = "message message-user";
     user_message.textContent = data["prompt"];
     chat_window.appendChild(user_message);
     chat.push({"role": "user", "content": data["prompt"]});
-    var asst_message = document.createElement("p");
+    asst_message = document.createElement("p");
     asst_message.className = "message message-asst";
     asst_message.textContent = "...";
     chat_window.appendChild(asst_message);
     chat_window.scrollTop = chat_window.scrollHeight;
+    send_button.disabled = true;
     try {
         const response = await fetch(query_url, 
             {
@@ -43,15 +56,18 @@ query_form.addEventListener("submit", async (event) => {
                 body: JSON.stringify({"chat": chat})
             }
         );
+        const data = await response.json();
         if (!response.ok) {
-            console.error(response.status);
+            delete_last_message();
             if (response.status == 401 || response.status == 403) {
                 api_key_status.textContent = "\u274C";
+            } else {
+                query_status.textContent = "Error: " + data.detail;
             }
         } else {
+            query_status.textContent = "";
             api_key_status.textContent = "\u2705";
-            const response_json = await response.json();
-            const response_text = response_json["chat"].at(-1)["content"];
+            const response_text = data["chat"].at(-1)["content"];
             chat.push({"role": "assistant", "content": response_text});
             asst_message.innerHTML = marked.parseInline(response_text);
             var chunks_used_message = document.createElement("p");
@@ -60,8 +76,9 @@ query_form.addEventListener("submit", async (event) => {
             chat_window.appendChild(chunks_used_message);
         }
     } catch (error) {
-        console.error(error);
+        console.log(error);
     }
+    send_button.disabled = false;
 });
 
 upload_form.addEventListener("submit", async (event) => {
@@ -70,6 +87,7 @@ upload_form.addEventListener("submit", async (event) => {
     upload_form.reset();
     const text = await file.text();
     upload_status.textContent = "Uploading...";
+    upload_button.disabled = true;
     try {
         const response = await fetch(upload_url, 
             {
@@ -81,15 +99,22 @@ upload_form.addEventListener("submit", async (event) => {
                 body: JSON.stringify({"text": text})
             }
         );
+        const data = await response.json();
         if (response.ok) {
             upload_status.textContent = "File successfully uploaded.";
+            api_key_status.textContent = "\u2705";
         } else {
-            upload_status.textContent = "Error uploading file.";
-            console.error(response.status);
+            if (response.status == 401 || response.status == 403) {
+                api_key_status.textContent = "\u274C";
+                upload_status.textContent = "";
+            } else {
+                upload_status.textContent = "Error uploading file: " + data.detail;
+            }
         }
     } catch (error) {
-        console.error(error);
+        console.log(error);
     }
+    upload_button.disabled = false;
 });
 
 api_key_input.addEventListener("input", (event) => {
